cfbfc65a21fe98e69b8c667660c141b853a32807
areybra <aretaradjawali@gmail.com>
2026-09-07 12:34:50 +0700

    fix(deploy): install PyMySQL as MySQLdb in wsgi.py before Django loads MySQL backend

--- config/wsgi.py
+++ config/wsgi.py
@@ -9,6 +9,16 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 
 import os
 
+# Install PyMySQL as MySQLdb BEFORE Django loads the MySQL backend
+# This must happen before Django loads the MySQL backend
+try:
+    import pymysql
+    pymysql.install_as_MySQLdb()
+except ImportError:
+    pass
+
+import os
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
